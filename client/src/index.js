8ea164083d7e66a131de70ef43c8d8f8efd98a88
SilasDemez <silasdemez@gmail.com>
2022-10-26 19:24:02 +0200

    ✨ add Login page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ export default function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home/>}></Route>
                     <Route path="register" element={<Register />} />
+                    <Route path="login" element={<Login />} />
                 </Route>
             </Routes>
         </BrowserRouter>
--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -9,7 +9,7 @@ const Layout = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
           </li>
           <li>
             <Link to="/register">Register</Link>
