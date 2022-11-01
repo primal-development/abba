61958f1d70e05adc533dffd06ec6796243a3280a
SilasDemez <silasdemez@gmail.com>
2022-11-01 17:36:37 +0100

    🚑 fix alert

--- client/src/pages/Confirm.js
+++ client/src/pages/Confirm.js
@@ -3,7 +3,19 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import ReactCodeInput from 'react-verification-code-input';
 import Axios from 'axios';
 import Alert from 'react-bootstrap/Alert';
-import AlertUser from './AlertUser';
+
+function AlertUser() {
+    return (
+        <Alert variant='danger'>
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+                You must type in the code you got sent to your email adress. Also try checking your spam folder!
+            </p>
+        </Alert>
+    )
+}
+
+
 
 function Confirm() {
 
@@ -13,13 +25,16 @@ function Confirm() {
     console.log(location.state);
     console.log("Authorization code: " + location.state.auth_code)
 
+    const [wrongCodeAlert, setwrongCodeAlert] = useState(false);
+
     const handleSubmit = (event) => {
 
         console.log(event);
 
         if (location.state.auth_code.toString() !== event.toString()) {
             console.log("Wrong!");
-        }else {
+            setwrongCodeAlert(!wrongCodeAlert);
+        } else {
             Axios.post("http://localhost:3001/createUser", {
                 athlete_name: location.state.athleteNameReg,
                 athlete_lastname: location.state.athleteLastnameReg,
@@ -41,7 +56,7 @@ function Confirm() {
         <div>
             <h2>We sent an email with a verification code to you</h2>
             <h6>Please verify your identity</h6>
-            {< AlertUser />}
+            {wrongCodeAlert && < AlertUser />}
             <ReactCodeInput onComplete={handleSubmit} />
 
             <h6 onClick={resendEmail} color={'#3590E4'} >I didn't receive an email. Send again</h6>
